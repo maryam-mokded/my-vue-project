9c6d7fce40f85408bcac06a004391eecd5fca71c
Mokded mariem <mariem.mokded@digitrends.dev>
2023-11-06 15:17:24 +0100

    First commit

---
+++ src/components/HomeTemplate.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="align-center text-center fill-height">
+      <h1>Test library project</h1>
+      <Steppers></Steppers>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script lang="ts" setup>
+  import  Steppers  from 'my-first-library'
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <HomeTemplate />
+</template>
+
+<script lang="ts" setup>
+  import HomeTemplate from '@/components/HomeTemplate.vue'
+</script>
