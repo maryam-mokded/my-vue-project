7c17f9578e926f41ccc8b45148a9a80b9cf56c4e
Mokded mariem <mariem.mokded@digitrends.dev>
2023-11-07 16:15:24 +0100

    Add && testing library

--- src/components/HomeTemplate.vue
+++ src/components/HomeTemplate.vue
@@ -2,11 +2,11 @@
   <v-container class="fill-height">
     <v-responsive class="align-center text-center fill-height">
       <h1>Test library project</h1>
-      <Steppers></Steppers>
+      <HomeLibrary></HomeLibrary>
     </v-responsive>
   </v-container>
 </template>
 
 <script lang="ts" setup>
-  import  Steppers  from 'my-first-library'
+import { HomeLibrary } from 'my-library'
 </script>
